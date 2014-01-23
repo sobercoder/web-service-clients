431231ce44acf0117dac39cb4e4ec272e400d8f5
Sandipan Das <sandipandas@mail.com>
2014-01-23 19:01:28 +0530

    Updated to fetch map image.
    Now saves a snapshot of the associated map in PNG format.

--- google-geocoding-client.cs
+++ google-geocoding-client.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Runtime.Serialization;
+using System.Windows.Controls;
 using System.Xml;
+
+using Newtonsoft.Json;
 
 #region "Data Contracts"
 
@@ -84,125 +88,114 @@ namespace Geocoding.Entities
 
 namespace Geocoding
 {
+	using System.IO;
 	using Geocoding.Entities;
+	using System.Text;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Media;
 	using System.Runtime.Serialization.Json;
 
 	class Boot
 	{
-		private static void showWeatherInfo(string woeId, string temperatureUnit = "c")
-		{
-			UriBuilder yahooWeatherUriBuilder = new UriBuilder("http://weather.yahooapis.com/forecastrss");
-			yahooWeatherUriBuilder.Query = string.Format("w={0}&u={1}", woeId,
-				(temperatureUnit.Equals("c") || temperatureUnit.Equals("f")) ? temperatureUnit : "c"
-			);
-
-			// Inject proxy credentials for transport through secured network (if any)
-			WebRequest.DefaultWebProxy = WebRequest.GetSystemWebProxy();
-			WebRequest.DefaultWebProxy.Credentials = CredentialCache.DefaultNetworkCredentials;
-
-			string yahooWeatherUrl = yahooWeatherUriBuilder.Uri.AbsoluteUri;
-
-			using (XmlReader yahooWeatherXmlReader = new XmlTextReader(yahooWeatherUrl))
-			{
-				XmlDocument yahooWeatherXmlDocument = new XmlDocument();
-				yahooWeatherXmlDocument.Load(yahooWeatherXmlReader);
-
-				// Set up namespace manager for XPath
-				XmlNamespaceManager yahooWeatherXmlNamespaceManager = new XmlNamespaceManager(yahooWeatherXmlDocument.NameTable);
-				yahooWeatherXmlNamespaceManager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
-
-				XmlNode yahooWeatherLocationNode = yahooWeatherXmlDocument.SelectSingleNode("/rss/channel/yweather:location", yahooWeatherXmlNamespaceManager);
-				XmlNode yahooWeatherUnitsNode = yahooWeatherXmlDocument.SelectSingleNode("/rss/channel/yweather:units", yahooWeatherXmlNamespaceManager);
-				XmlNode yahooWeatherWindNode = yahooWeatherXmlDocument.SelectSingleNode("/rss/channel/yweather:wind", yahooWeatherXmlNamespaceManager);
-				XmlNode yahooWeatherAtmosphereNode = yahooWeatherXmlDocument.SelectSingleNode("/rss/channel/yweather:atmosphere", yahooWeatherXmlNamespaceManager);
-				XmlNode yahooWeatherAstronomyNode = yahooWeatherXmlDocument.SelectSingleNode("/rss/channel/yweather:astronomy", yahooWeatherXmlNamespaceManager);
-				XmlNodeList yahooWeatherForecaseNodes = yahooWeatherXmlDocument.SelectNodes("/rss/channel/item/yweather:forecast", yahooWeatherXmlNamespaceManager);
-
-				Console.WriteLine("Location: [city: {0}], [region: {1}], [country: {2}]",
-					yahooWeatherLocationNode.Attributes["city"].InnerText,
-					yahooWeatherLocationNode.Attributes["region"].InnerText,
-					yahooWeatherLocationNode.Attributes["country"].InnerText
-				);
-
-				Console.WriteLine("Wind: [chill: {0}], [direction: {1}], [speed: {2}]",
-					yahooWeatherWindNode.Attributes["chill"].InnerText,
-					yahooWeatherWindNode.Attributes["direction"].InnerText,
-					yahooWeatherWindNode.Attributes["speed"].InnerText + yahooWeatherUnitsNode.Attributes["speed"].InnerText
-				);
-
-				Console.WriteLine("Atmosphere: [humidity: {0}], [visibility: {1}], [pressure: {2}]",
-					yahooWeatherAtmosphereNode.Attributes["humidity"].InnerText,
-					yahooWeatherAtmosphereNode.Attributes["visibility"].InnerText + yahooWeatherUnitsNode.Attributes["distance"].InnerText,
-					yahooWeatherAtmosphereNode.Attributes["pressure"].InnerText + yahooWeatherUnitsNode.Attributes["pressure"].InnerText
-				);
-
-				Console.WriteLine("Astronomy: [sunrise: {0}], [sunset: {1}]",
-					yahooWeatherAstronomyNode.Attributes["sunrise"].InnerText,
-					yahooWeatherAstronomyNode.Attributes["sunset"].InnerText
-				);
-
-				foreach (XmlNode yahooWeatherForecaseNode in yahooWeatherForecaseNodes)
-				{
-					Console.WriteLine("Forecast for [{0}]: [date: {1}], [low: {2}], [high: {3}], [text: {4}]",
-						yahooWeatherForecaseNode.Attributes["day"].InnerText,
-						yahooWeatherForecaseNode.Attributes["date"].InnerText,
-						yahooWeatherForecaseNode.Attributes["low"].InnerText,
-						yahooWeatherForecaseNode.Attributes["high"].InnerText,
-						yahooWeatherForecaseNode.Attributes["text"].InnerText
-					);
-				}
-			}
-		}
-
 		public static void Main(string[] args)
 		{
 			const string googleGeocodingBaseUrl = "http://maps.googleapis.com/maps/api/geocode/json";
-			string address = string.Empty;
+			const string googleStaticMapsBaseUrl = "http://maps.googleapis.com/maps/api/staticmap";
+			string paramAddress = string.Empty;
 
 			Console.Write("Address? ");
-			address = Console.ReadLine();
+			paramAddress = Console.ReadLine();
 
-			if (string.IsNullOrWhiteSpace(address))
+			if (string.IsNullOrWhiteSpace(paramAddress))
 			{
 				return;
 			}
 
 			UriBuilder googleGeocodingUriBuilder = new UriBuilder(googleGeocodingBaseUrl);
-			googleGeocodingUriBuilder.Query = string.Format("address={0}&sensor=false", Uri.EscapeUriString(address));
+			googleGeocodingUriBuilder.Query = string.Format("address={0}&sensor=false", Uri.EscapeUriString(paramAddress));
 
 			// Inject proxy credentials for transport through secured network (if any)
 			WebRequest.DefaultWebProxy = WebRequest.GetSystemWebProxy();
 			WebRequest.DefaultWebProxy.Credentials = CredentialCache.DefaultNetworkCredentials;
 
+			Response googleGeocodingJsonResponse = null;
 			HttpWebRequest googleGeocodingRequest = WebRequest.Create(googleGeocodingUriBuilder.Uri) as HttpWebRequest;
 
 			using (HttpWebResponse googleGeocodingResponse = googleGeocodingRequest.GetResponse() as HttpWebResponse)
 			{
-				if (googleGeocodingResponse.StatusCode == HttpStatusCode.OK)
+				if (googleGeocodingResponse.StatusCode != HttpStatusCode.OK)
 				{
-					DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
-					Response googleGeocodingJsonResponse = jsonSerializer.ReadObject(googleGeocodingResponse.GetResponseStream()) as Response;
+					return;
+				}
 
-					foreach (Result result in googleGeocodingJsonResponse.Results)
+				DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
+				googleGeocodingJsonResponse = jsonSerializer.ReadObject(googleGeocodingResponse.GetResponseStream()) as Response;
+
+				if (googleGeocodingResponse == null)
+				{
+					return;
+				}
+
+				foreach (Result result in googleGeocodingJsonResponse.Results)
+				{
+					foreach (AddressComponent addrComponent in result.AddressComponents)
 					{
-						foreach (AddressComponent addrComponent in result.AddressComponents)
-						{
-							Console.WriteLine("Long Name: {0}, ShortName: {1}, Types: {2}", addrComponent.LongName, addrComponent.ShortName, string.Join(",", addrComponent.Types));
-						}
-
-						Console.WriteLine("Formatted Address: {0}", result.FormattedAddress);
-						Console.WriteLine("Geometry: [Location: [Latitude: {0}], [Longitude: {1}]], [Location Type: {2}], [Viewport: [North East: [Latitude: {3}], [Longitude: {4}]], [South West: [Latitude: {5}], [Longitude: {6}]]]",
-							result.Geometry.Location.Latitude,
-							result.Geometry.Location.Longitude,
-							result.Geometry.LocationType,
-							result.Geometry.Viewport.NorthEast.Latitude,
-							result.Geometry.Viewport.NorthEast.Longitude,
-							result.Geometry.Viewport.SouthWest.Latitude,
-							result.Geometry.Viewport.SouthWest.Longitude
-						);
+						Console.WriteLine("Long Name: {0}, ShortName: {1}, Types: {2}", addrComponent.LongName, addrComponent.ShortName, string.Join(", ", addrComponent.Types));
 					}
-					
-					Console.WriteLine(googleGeocodingJsonResponse.Status);
+
+					Console.WriteLine("Formatted Address: {0}", result.FormattedAddress);
+					Console.WriteLine("Geometry: [Location: [Latitude: {0}], [Longitude: {1}]], [Location Type: {2}], [Viewport: [North East: [Latitude: {3}], [Longitude: {4}]], [South West: [Latitude: {5}], [Longitude: {6}]]]",
+						result.Geometry.Location.Latitude,
+						result.Geometry.Location.Longitude,
+						result.Geometry.LocationType,
+						result.Geometry.Viewport.NorthEast.Latitude,
+						result.Geometry.Viewport.NorthEast.Longitude,
+						result.Geometry.Viewport.SouthWest.Latitude,
+						result.Geometry.Viewport.SouthWest.Longitude
+					);
+				}
+
+				Console.WriteLine("Status: {0}", googleGeocodingJsonResponse.Status);
+			}
+
+			if (googleGeocodingJsonResponse.Results.Length > 0)
+			{
+				Result googleGeocodingResult = googleGeocodingJsonResponse.Results.First();
+				string paramCenter = googleGeocodingResult.FormattedAddress;
+				int paramZoom = 14;
+				string paramSize = "500x500";
+
+				UriBuilder googleStaticMapsUriBuilder = new UriBuilder(googleStaticMapsBaseUrl);
+				googleStaticMapsUriBuilder.Query = string.Format("center={0}&zoom={1}&size={2}&sensor=false", Uri.EscapeUriString(paramCenter), paramZoom, Uri.EscapeUriString(paramSize));
+
+				HttpWebRequest googleStaticMapsRequest = WebRequest.Create(googleStaticMapsUriBuilder.Uri) as HttpWebRequest;
+
+				using (HttpWebResponse googleStaticMapsResponse = googleStaticMapsRequest.GetResponse() as HttpWebResponse)
+				{
+					if (googleStaticMapsResponse.StatusCode == HttpStatusCode.OK)
+					{
+						string paramPngFile = string.Format("{0}-{1}-{2}.png", Uri.EscapeUriString(paramCenter), paramZoom, paramSize);
+
+						using (BufferedStream googleStaticMapsResponseBufferedStream = new BufferedStream(googleStaticMapsResponse.GetResponseStream()))
+						{
+							if (googleStaticMapsResponseBufferedStream.CanRead)
+							{
+								PngBitmapDecoder googleStaticMapsImageDecoder = new PngBitmapDecoder(googleStaticMapsResponseBufferedStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+								BitmapSource googleStaticMapsImageBitmapSource = googleStaticMapsImageDecoder.Frames[0];
+
+								if (File.Exists(paramPngFile))
+								{
+									File.Delete(paramPngFile);
+								}
+
+								FileStream googleStaticMapsPngStream = new FileStream(paramPngFile, FileMode.CreateNew);
+								PngBitmapEncoder encoder = new PngBitmapEncoder();
+								encoder.Interlace = PngInterlaceOption.On;
+								encoder.Frames.Add(BitmapFrame.Create(googleStaticMapsImageBitmapSource));
+								encoder.Save(googleStaticMapsPngStream);
+							}
+						}
+					}
 				}
 			}
 
